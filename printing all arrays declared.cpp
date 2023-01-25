77ac7bb3300a1b28363f1a0b60982b4a7e22d0ac
hafsacore <110922795+hafsacore@users.noreply.github.com>
2023-01-25 11:05:15 +0500

    Add files via upload

---
+++ printing all arrays declared.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+    int main()
+    {
+    string users[6] = {"abu, ami, aamna, aateka, hafsa, maria"};
+    
+    for(int i=0; i<=5; i++) {
+        cout<<users[i];
+    }
+    
+    return 0;
+}
